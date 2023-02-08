c08f0426f8b6bc490da56aef3b93300d64584090
nickhaugen19 <71619276+nickhaugen19@users.noreply.github.com>
2023-02-08 16:22:31 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
-function changecolor(){  
- alert("Hello Grader!");  
-}  
+function changeColor(color) {
+ document.body.style.background = color;
+        }
+          
+function changeColorButton() {
+ changeColor('red');
+ 
+}
