ed5825ae036e767667cbf3fd2d7f496d4a10e814
nickhaugen19 <71619276+nickhaugen19@users.noreply.github.com>
2023-02-08 15:53:08 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,21 @@
-<button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
+// Event Handler For Click
+function submitClicked() {
+  
+  let vals = document.getElementsByTagName("input");
+  
+  console.log(vals.length);
+  
+  for (let i = 0; i < vals.length; i++){
+    
+   
+      if(vals[i].checked) {
+        console.log(vals[i].value);
+        
+        let newText = "Selected Vis: " + vals[i].value;
+        document.getElementById("selected-vis").innerHTML = newText;
+      }
+  }
+}
 
-<p id="demo"></p>
+document.getElementById("subButton")
+        .addEventListener("click", submitClicked);
